6496bd2e49ca6435ee1937e87e648869c641bfc8
unknown <Yauheni_Novik@EPBYGOMW0234.gomel.epam.com>
2014-01-29 13:51:52 +0300

    add direct copy from file to file

---
+++ src/main/java/com/epam/novik/task1/App.java
@@ -0,0 +1,17 @@
+package com.epam.novik.task1;
+
+/**
+ * Hello world!
+ * 
+ */
+public class App {
+	private static org.apache.log4j.Logger log = org.apache.log4j.Logger
+			.getLogger(App.class);
+
+	public static void main(String[] args) {
+		log.info("Hello Yauheni Novik!");
+		System.out.println("adding System.out.println()");
+		System.out.println("adding second comment into branch");
+		System.out.println("adding third comment into branch");
+	}
+}
